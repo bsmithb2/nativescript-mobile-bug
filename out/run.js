7be282fcc6c12b07779e9599e517710c2c3a0567
Bradley Smith <bsmithb2@gmail.com>
2019-02-10 10:00:14 +1100

    Added error scenarios

--- out/run.js
+++ out/run.js
@@ -1,2 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var mobile_devices_controller_1 = require("mobile-devices-controller");
+var android_home = mobile_devices_controller_1.AndroidController.getAndroidHome();
+console.log("android_home = " + android_home);
+var p = mobile_devices_controller_1.AndroidController.parsePlatforms();
+console.log('platforms: ');
+console.dir(p);
+var avds = mobile_devices_controller_1.AndroidController.parseEmulatorsAvds();
+console.log('platforms: ' + JSON.stringify(avds));
 console.log('output');
 //# sourceMappingURL=run.js.map
--- run.ts
+++ run.ts
@@ -1,1 +1,17 @@
+import { AndroidController } from 'mobile-devices-controller';
+
+var android_home = AndroidController.getAndroidHome();
+console.log("android_home = " + android_home);
+
+
+var p = AndroidController.parsePlatforms();
+console.log('platforms: ');
+console.dir(p);
+//android-27
+//android-28
+
+var  avds = AndroidController.parseEmulatorsAvds();
+//when avds include "Android_Accelerated_Oreo.ini" then crash. 
+
+console.log('platforms: ' + JSON.stringify(avds));  
 console.log('output');
